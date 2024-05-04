add67402e4032bbde5927f12397fa20a927570b7
Lecky Lao <leckylao@gmail.com>
2024-05-05 00:49:24 +1000

    Add aggregator

---
+++ aggregator.go
@@ -0,0 +1,149 @@
+package aggregator
+
+import (
+	"context"
+	"math/big"
+	"sync"
+	"time"
+)
+
+const (
+	// number of blocks after which a task is considered expired
+	// this hardcoded here because it's also hardcoded in the contracts, but should
+	// ideally be fetched from the contracts
+	taskChallengeWindowBlock = 100
+	blockTimeSeconds         = 12 * time.Second
+)
+
+type Aggregator struct {
+	logger           logging.Logger
+	serverIpPortAddr string
+	// aggregation related fields
+	blsAggregationService blsagg.BlsAggregationService
+	tasksMu               sync.RWMutex
+	taskResponsesMu       sync.RWMutex
+}
+
+// NewAggregator creates a new Aggregator with the provided config.
+func NewAggregator(c *config.Config) (*Aggregator, error) {
+
+	chainioConfig := sdkclients.BuildAllConfig{
+		EthHttpUrl:                 c.EthHttpRpcUrl,
+		EthWsUrl:                   c.EthWsRpcUrl,
+		OperatorStateRetrieverAddr: c.OperatorStateRetrieverAddr.String(),
+		PromMetricsIpPortAddress:   ":9090",
+	}
+	clients, err := clients.BuildAll(chainioConfig, c.EcdsaPrivateKey, c.Logger)
+	if err != nil {
+		c.Logger.Errorf("Cannot create sdk clients", "err", err)
+		return nil, err
+	}
+
+	return &Aggregator{
+		logger:                c.Logger,
+		serverIpPortAddr:      c.AggregatorServerIpPortAddr,
+		blsAggregationService: blsAggregationService,
+	}, nil
+}
+
+func (agg *Aggregator) Start(ctx context.Context) error {
+	agg.logger.Infof("Starting aggregator.")
+	agg.logger.Infof("Starting aggregator rpc server.")
+	go agg.startServer(ctx)
+
+	// TODO(soubhik): refactor task generation/sending into a separate function that we can run as goroutine
+	ticker := time.NewTicker(10 * time.Second)
+	agg.logger.Infof("Aggregator set to send new task every 10 seconds...")
+	defer ticker.Stop()
+	taskNum := int64(0)
+	// ticker doesn't tick immediately, so we send the first task here
+	// see https://github.com/golang/go/issues/17601
+	_ = agg.sendNewTask(big.NewInt(taskNum))
+	taskNum++
+
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case blsAggServiceResp := <-agg.blsAggregationService.GetResponseChannel():
+			agg.logger.Info("Received response from blsAggregationService", "blsAggServiceResp", blsAggServiceResp)
+			agg.sendAggregatedResponseToContract(blsAggServiceResp)
+		case <-ticker.C:
+			err := agg.sendNewTask(big.NewInt(taskNum))
+			taskNum++
+			if err != nil {
+				// we log the errors inside sendNewTask() so here we just continue to the next task
+				continue
+			}
+		}
+	}
+}
+
+func (agg *Aggregator) sendAggregatedResponseToContract(blsAggServiceResp blsagg.BlsAggregationServiceResponse) {
+	// TODO: check if blsAggServiceResp contains an err
+	if blsAggServiceResp.Err != nil {
+		agg.logger.Error("BlsAggregationServiceResponse contains an error", "err", blsAggServiceResp.Err)
+		// panicing to help with debugging (fail fast), but we shouldn't panic if we run this in production
+		panic(blsAggServiceResp.Err)
+	}
+	nonSignerPubkeys := []cstaskmanager.BN254G1Point{}
+	for _, nonSignerPubkey := range blsAggServiceResp.NonSignersPubkeysG1 {
+		nonSignerPubkeys = append(nonSignerPubkeys, core.ConvertToBN254G1Point(nonSignerPubkey))
+	}
+	quorumApks := []cstaskmanager.BN254G1Point{}
+	for _, quorumApk := range blsAggServiceResp.QuorumApksG1 {
+		quorumApks = append(quorumApks, core.ConvertToBN254G1Point(quorumApk))
+	}
+	nonSignerStakesAndSignature := cstaskmanager.IBLSSignatureCheckerNonSignerStakesAndSignature{
+		NonSignerPubkeys:             nonSignerPubkeys,
+		QuorumApks:                   quorumApks,
+		ApkG2:                        core.ConvertToBN254G2Point(blsAggServiceResp.SignersApkG2),
+		Sigma:                        core.ConvertToBN254G1Point(blsAggServiceResp.SignersAggSigG1.G1Point),
+		NonSignerQuorumBitmapIndices: blsAggServiceResp.NonSignerQuorumBitmapIndices,
+		QuorumApkIndices:             blsAggServiceResp.QuorumApkIndices,
+		TotalStakeIndices:            blsAggServiceResp.TotalStakeIndices,
+		NonSignerStakeIndices:        blsAggServiceResp.NonSignerStakeIndices,
+	}
+
+	agg.logger.Info("Threshold reached. Sending aggregated response onchain.",
+		"taskIndex", blsAggServiceResp.TaskIndex,
+	)
+	agg.tasksMu.RLock()
+	task := agg.tasks[blsAggServiceResp.TaskIndex]
+	agg.tasksMu.RUnlock()
+	agg.taskResponsesMu.RLock()
+	taskResponse := agg.taskResponses[blsAggServiceResp.TaskIndex][blsAggServiceResp.TaskResponseDigest]
+	agg.taskResponsesMu.RUnlock()
+	if err != nil {
+		agg.logger.Error("Aggregator failed to respond to task", "err", err)
+	}
+}
+
+// sendNewTask sends a new task to the task manager contract, and updates the Task dict struct
+// with the information of operators opted into quorum 0 at the block of task creation.
+func (agg *Aggregator) sendNewTask(numToSquare *big.Int) error {
+	agg.logger.Info("Aggregator sending new task", "numberToSquare", numToSquare)
+	// Send number to square to the task manager contract
+	if err != nil {
+		agg.logger.Error("Aggregator failed to send number to square", "err", err)
+		return err
+	}
+
+	agg.tasksMu.Lock()
+	agg.tasks[taskIndex] = newTask
+	agg.tasksMu.Unlock()
+
+	quorumThresholdPercentages := make(sdktypes.QuorumThresholdPercentages, len(newTask.QuorumNumbers))
+	for i := range newTask.QuorumNumbers {
+		quorumThresholdPercentages[i] = sdktypes.QuorumThresholdPercentage(newTask.QuorumThresholdPercentage)
+	}
+	// TODO(samlaf): we use seconds for now, but we should ideally pass a blocknumber to the blsAggregationService
+	// and it should monitor the chain and only expire the task aggregation once the chain has reached that block number.
+	taskTimeToExpiry := taskChallengeWindowBlock * blockTimeSeconds
+	var quorumNums sdktypes.QuorumNums
+	for _, quorumNum := range newTask.QuorumNumbers {
+		quorumNums = append(quorumNums, sdktypes.QuorumNum(quorumNum))
+	}
+	agg.blsAggregationService.InitializeNewTask(taskIndex, newTask.TaskCreatedBlock, quorumNums, quorumThresholdPercentages, taskTimeToExpiry)
+	return nil
+}
